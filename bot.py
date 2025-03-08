9a792caeb9232f15aa8635d3ae8c65b04a630604
xaxp <tomas.peterka@gmail.com>
2025-03-08 08:50:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import pybit
 from pybit.unified_trading import HTTP
 
-api_key = "Hju6xSgraXdq0htbaGLd"      # tvůj API klíč v uvozovkách
-api_secret = "LeenaAsr4(...)"          # tvůj API Secret v uvozovkách
+api_key = "85WQVHsc8epCLTLcdm"      # tvůj API klíč v uvozovkách
+api_secret = "9kkbVM8oasMB9mGOgmD4lB7kAVx44YSKGIri"          # tvůj API Secret v uvozovkách
 
 session = HTTP(api_key=api_key, api_secret=api_secret, testnet=True)
 
